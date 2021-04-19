1fba123ec254873b26c9fb1aef4a0cd13ba73bf5
Beatrice Castagno <beatrice.castagno1@gmail.com>
2021-04-20 00:38:10 +0200

    Aggiunta sezione icone luxury + effetto wave div

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
 var app = new Vue ({
   el: "#app",
   data: {
-    
+    icons: [
+      {
+        pathIcon: "fas fa-sign-language",
+        text: "Massages"
+      },
+      {
+        pathIcon: "far fa-smile",
+        text: "Therapy"
+      },
+      {
+        pathIcon: "fas fa-heart",
+        text: "Relaxing"
+      },
+      {
+        pathIcon: "far fa-moon",
+        text: "Meditation"
+      },
+      {
+        pathIcon: "far fa-snowflake",
+        text: "Natural Beauty"
+      }
+    ]
   }
 });
