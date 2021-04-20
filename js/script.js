7dda6625b1205b775f4f1485155da11998cec1fd
Beatrice Castagno <beatrice.castagno1@gmail.com>
2021-04-20 18:37:21 +0200

    Aggiunta sezione scelta trattamento

--- js/script.js
+++ js/script.js
@@ -22,6 +22,41 @@ var app = new Vue ({
         pathIcon: "far fa-snowflake",
         text: "Natural Beauty"
       }
-    ]
+    ],
+    showAddOption: false,
+    chooseTreatments: [
+      {
+        name: "Oil Massage",
+        show: false,
+        optionAdd: {
+          price: "$62",
+          time: "For 2 Hours",
+          description: "Lorem ipsum dolor sit amet ipsum dolor"
+        }
+      },
+      {
+        name: "Manicure",
+        show: false,
+        optionAdd: {
+          price: "$55",
+          time: "",
+          description: "Lorem ipsum dolor sit amet ipsum dolor sit amet"
+        }
+      },
+      {
+        name: "Aroma Therapy",
+        show: false,
+        optionAdd: {
+          price: "$75",
+          time: "For 1 Hours",
+          description: "Lorem ipsum dolor sit amet"
+        }
+      }
+    ],
+  },
+  methods: {
+    // showOption: function(element) {
+    // 	element.show = !element.show;
+    // },
   }
 });
